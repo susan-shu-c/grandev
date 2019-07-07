e3949392e755b0e166276f8977e8949cc379ab1d
Susan Chang <susan.chang@edu.uwaterloo.ca>
2019-07-06 20:58:55 -0400

    Add crawler files

---
+++ imagespider/imagespider/spiders/crawler.py
@@ -0,0 +1,26 @@
+from imagespider.items import ImagespiderItem
+import datetime
+import scrapy
+
+class Crawler(scrapy.Spider):
+    name = "image_spider"
+    pages = 0
+    start_urls = ["https://www.gettyimages.ca/photos/ariana-grande?family=editorial&phrase=ariana%20grande&sort=best#license"]
+
+    def parse(self, response):
+        Crawler.pages+=1
+        url = response.xpath('//*[@id="assets"]/article')
+        data = url.xpath('//*[@="assets"]/article').extract()
+
+        for href in data:
+            imglink = href[href.find('src'):]
+            imglink = imglink[5:imglink.find('>')-1]
+            title = href[href.find('target'):]
+            title = title[8:title.find('>')-1]
+            now = datetime.datetime.now()
+            yield MyImage(title=title, createDate=now, image_urls=[imglink])
+
+        if Crawler.pages !=2:
+            next = response.xpath('//*[@id="next-gallery-page"]')
+            if next is not None:
+                yield scrapy.Request("https://www.gettyimages.com"+next.xpath("@href").extract_first(), self.parse)
