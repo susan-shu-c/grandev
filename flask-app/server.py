80c533a07b6e4087b1b501c5def05bb613869a7c
Susan Chang <shu.chang@alum.utoronto.ca>
2019-07-14 17:24:09 -0400

    Initial commit of Flask app portion

---
+++ flask-app/server.py
@@ -0,0 +1,44 @@
+import os
+from flask import Flask, flash, render_template, redirect, request, url_for
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "supertopsecretprivatekey"
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'GET':
+        # show the upload form
+        return render_template('home.html')
+
+    if request.method == 'POST':
+        # check if a file was passed into the POST request
+        if 'image' not in request.files:
+            flash('No file was uploaded.')
+            return redirect(request.url)
+
+        image_file = request.files['image']
+
+        # if filename is empty, then assume no upload
+        if image_file.filename == '':
+            flash('No file was uploaded.')
+            return redirect(request.url)
+
+        # if the file is "legit"
+        if image_file:
+            passed = False
+            try:
+                filename = image_file.filename
+                filepath = os.path.join('/tmp/fuzzvis/', filename)
+                image_file.save(filepath)
+                passed = True
+            except Exception:
+                passed = False
+
+            if passed:
+                return redirect(url_for('predict', filename=filename))
+            else:
+                flash('An error occurred, try again.')
+                return redirect(request.url)
+
+...
+
