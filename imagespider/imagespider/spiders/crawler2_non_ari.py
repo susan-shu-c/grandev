2017850c9c9df5fb696d7f966979ae4022be1592
Susan Chang <shu.chang@alum.utoronto.ca>
2019-07-21 11:21:51 -0400

    Add crawler for non ari images

---
+++ imagespider/imagespider/spiders/crawler2_non_ari.py
@@ -0,0 +1,28 @@
+from imagespider.items import ImagespiderItem
+import datetime
+import scrapy
+
+class Crawler(scrapy.Spider):
+    name = "image_spider2_non_ari"
+    pages = 0 
+    page_limit = 10 # pages to scrape
+    start_urls = ["https://www.gettyimages.ca/search/2/image?family=editorial&phrase=&sort=mostpopular#license"]
+
+    def parse(self, response):
+        Crawler.pages+=1
+        # url = response.xpath('//*[@id="assets"]/article')
+        data = response.css('img').xpath('@src').getall() # this should be list of urls (string)
+
+        for href in data:
+            title = data.index(href) # The element in list
+            imglink = href
+            now = datetime.datetime.now()
+            yield ImagespiderItem(title=title, createDate=now, image_urls=[imglink])
+
+        if Crawler.pages < Crawler.page_limit:
+            yield scrapy.Request("https://www.gettyimages.ca/search/2/image?family=editorial&page=" + str(Crawler.pages) + "&phrase=&sort=mostpopular#license", self.parse)
+
+        # if Crawler.pages !=2:
+            # next = response.xpath('//*[@id="next-gallery-page"]')
+            # if next is not None:
+                # yield scrapy.Request("https://www.gettyimages.com"+next.xpath("@href").extract_first(), self.parse)
